eb2df8b343042638a81999d52009ba704cec5446
John Holl <hzylyh@hotmail.com>
2019-08-16 10:29:36 +0800

    1.添加拦截器

--- src/main/java/com/easymarket/conf/MyInterceptor.java
+++ src/main/java/com/easymarket/conf/MyInterceptor.java
@@ -1,16 +1,20 @@
 package com.easymarket.conf;
 
+import com.easymarket.utils.JWTUtil;
+import com.easymarket.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 @Component
 public class MyInterceptor implements HandlerInterceptor {
-//    @Autowired
-//    private RedisUtil redisUtil;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -19,22 +23,22 @@ public class MyInterceptor implements HandlerInterceptor {
         String reqToken = request.getHeader("Authorization");
 
 
-//        if (reqToken != null) {
-//            String userId = JWTUtil.getUserId(reqToken);
-//            String tokenInRedis = (String) redisUtil.get(userId);
-//            if (tokenInRedis != null && tokenInRedis.equals(reqToken)) {
-//                return true;
-//            } else {
-//                PrintWriter printWriter = response.getWriter();
-//                printWriter.write("{\"code\":\"21001\",\"message\":\"not login!\"}");
-//                return false;
-//            }
-//        }else {
-//            PrintWriter printWriter = response.getWriter();
-//            printWriter.write("{\"code\":\"21001\",\"message\":\"not login!\"}");
-//            return false;
-//        }
-        return true;
+        if (reqToken != null) {
+            String userId = JWTUtil.getUserId(reqToken);
+            String tokenInRedis = (String) redisUtil.get(userId);
+            if (tokenInRedis != null && tokenInRedis.equals(reqToken)) {
+                return true;
+            } else {
+                PrintWriter printWriter = response.getWriter();
+                printWriter.write("{\"code\":\"21001\",\"message\":\"not login!\"}");
+                return false;
+            }
+        }else {
+            PrintWriter printWriter = response.getWriter();
+            printWriter.write("{\"code\":\"21001\",\"message\":\"not login!\"}");
+            return false;
+        }
+//        return true;
 
     }
 
