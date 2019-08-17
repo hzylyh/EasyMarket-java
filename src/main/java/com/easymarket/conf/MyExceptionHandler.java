63fd2ca98f5a19172e9c39f0f93dec576d44639b
John Holl <hzylyh@hotmail.com>
2019-08-17 16:35:11 +0800

    1.优化

--- src/main/java/com/easymarket/conf/MyExceptionHandler.java
+++ src/main/java/com/easymarket/conf/MyExceptionHandler.java
@@ -3,28 +3,29 @@ package com.easymarket.conf;
 
 import com.easymarket.entity.Response;
 import com.easymarket.utils.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class MyExceptionHandler {
 
     @ExceptionHandler(value = MyException.class)
     public Response errHandler(MyException e) {
-        e.printStackTrace();
+        log.error("", e);
         return ResponseUtil.error(e.getErrCode(), e.getErrMsg());
     }
 
     @ExceptionHandler(value = ArithmeticException.class)
     public Response errHandler(ArithmeticException e) {
-        e.printStackTrace();
+        log.error("", e);
         return ResponseUtil.error("123", "14414");
     }
 
     @ExceptionHandler(value = NullPointerException.class)
     public Response errHandler(NullPointerException e) {
-        e.printStackTrace();
+        log.error("", e);
         return ResponseUtil.error("123", "空指针了");
     }
 }
